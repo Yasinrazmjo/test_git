546a77bb6fdc29ce113bad7fe81e118f43811e5f
Yasin razmjo <yrz51081@gmail.com>
2026-02-27 00:04:09 -0800

    I add a mothod to correct.py

--- correct.py
+++ correct.py
@@ -1,1 +1,14 @@
-print('Hel')
+counter = 0
+for i in range(2,10):
+    for j in range(2,i-1):
+        if(j != 1):
+            if(i % j == 0):
+                counter += 1
+    if(counter == 0):
+        print(f'{i} is Aval')
+    counter = 0
+    
+print('Good job, you found Avl number in range (0,10)')
+                
+    
+    
